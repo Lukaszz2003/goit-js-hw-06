e8ca5170677eebef52a251fe14b6bcec902f3650
Łukasz Paradowski <123119119+Lukaszz2003@users.noreply.github.com>
2023-05-27 10:31:26 +0200

    new

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -1,1 +1,14 @@
-const counterValue = 0;
+let counterValue = 0;
+
+const newValue = document.querySelector("#value");
+const buttonDecrement = document.querySelector("button[data-action=decrement]");
+const buttonIncrement = document.querySelector("button[data-action=increment]");
+
+buttonDecrement.addEventListener("click", () => {
+  counterValue -= 1;
+  document.getElementById("value").textContent = counterValue;
+});
+buttonIncrement.addEventListener("click", () => {
+  counterValue += 1;
+  document.getElementById("value").textContent = counterValue;
+});
