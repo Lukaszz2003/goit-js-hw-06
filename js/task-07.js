e0915a7c772e54cbe8bb8752a4eb252ce164b2d9
Łukasz Paradowski <123119119+Lukaszz2003@users.noreply.github.com>
2023-05-30 14:53:47 +0200

    new

--- js/task-07.js
+++ js/task-07.js
@@ -1,7 +1,7 @@
 const inputChange = document.querySelector("input#font-size-control");
 const abracadabra = document.querySelector("span#text");
 
-inputChange.addEventListener("change", handleInputRange);
+inputChange.addEventListener("input", handleInputRange);
 function handleInputRange(event) {
   abracadabra.style.fontSize = `${event.target.value}px`;
 }
