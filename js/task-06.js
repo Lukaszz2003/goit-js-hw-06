e16267b8bf104cd9983a8091d9c397814dd1642b
Łukasz Paradowski <123119119+Lukaszz2003@users.noreply.github.com>
2023-05-30 14:33:27 +0200

    new

--- js/task-06.js
+++ js/task-06.js
@@ -1,10 +1,10 @@
-document.getElementById("validation-input").onblur = function () {
-  console.log(this.value.length);
-  if (this.getAttribute("data-length") > this.value.length) {
-    this.classList.remove("valid");
-    this.classList.add("invalid");
-  } else {
-    this.classList.remove("invalid");
-    this.classList.add("valid");
-  }
-};
+const input = document.querySelector("input#validation-input");
+input.addEventListener("blur", (e) => {
+  if (
+    e.currentTarget.value.length ===
+    parseInt(e.currentTarget.getAttribute("data-length"))
+  )
+    e.currentTarget.setAttribute("class", "valid");
+  else e.currentTarget.setAttribute("class", "invalid");
+});
+console.log(parseInt(input.getAttribute("data-length")));
